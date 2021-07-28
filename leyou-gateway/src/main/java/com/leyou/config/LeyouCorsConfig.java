72f695c130f26fc814393ef0b91d6e21bcc6f5d5
cranekong <2686261905@qq.com>
2021-07-28 14:49:53 +0800

    springboot跨域cros修改

--- leyou-gateway/src/main/java/com/leyou/config/LeyouCorsConfig.java
+++ leyou-gateway/src/main/java/com/leyou/config/LeyouCorsConfig.java
@@ -1,5 +1,6 @@
 package com.leyou.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,11 +10,13 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class LeyouCorsConfig {
     @Bean
-    public CorsFilter corsFilter() {
+    public FilterRegistrationBean corsFilter() {
         //1.添加CORS配置信息
         CorsConfiguration config = new CorsConfiguration();
         //1) 允许的域,不要写*，否则cookie就无法使用了
+// config.addAllowedOrigin("http://localhost:8081");
         config.addAllowedOrigin("http://manage.leyou.com");
+        config.addAllowedOrigin("*");
         //2) 是否发送Cookie信息
         config.setAllowCredentials(true);
         //3) 允许的请求方式
@@ -24,6 +27,7 @@ public class LeyouCorsConfig {
         config.addAllowedMethod("POST");
         config.addAllowedMethod("DELETE");
         config.addAllowedMethod("PATCH");
+        config.setMaxAge(3600L);
         // 4）允许的头信息
         config.addAllowedHeader("*");
 
@@ -31,7 +35,8 @@ public class LeyouCorsConfig {
         UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
         configSource.registerCorsConfiguration("/**", config);
 
-        //3.返回新的CorsFilter.
-        return new CorsFilter(configSource);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(configSource));
+        bean.setOrder(0);//利用FilterRegistrationBean，将拦截器注册靠前,避免被其它拦截器首先执行
+        return bean;
     }
 }
